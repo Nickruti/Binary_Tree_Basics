faef180bcc97947e20eb2413bacf446ab0180d14
Kruti Patel <37500239+Nickruti@users.noreply.github.com>
2020-12-27 11:31:15 +0530

    Create Symmetric-tree.py

---
+++ Symmetric-tree.py
@@ -0,0 +1,48 @@
+'''
+Problem url - 
+'''
+
+#---------------------------- Recursive Solution ------------------------------
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def helper(node1, node2):
+            if node1 is None and node2 is None:
+                return True
+            if node1 is None or node2 is None:
+                return False
+            return (node1.val == node2.val) and helper(node1.right, node2.left) and helper(node1.left, node2.right)
+        
+        return helper(root, root)
+        
+ #------------------ Iterative Solution - little better than recursive -----------------------
+ # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        queue = []
+        queue.append(root)
+        queue.append(root)
+        while queue:
+            t1 = queue.pop(0)
+            t2 = queue.pop(0)
+            if t1 is None and t2 is None:
+                continue
+            if t1 is None or t2 is None:
+                return False
+            if t1.val != t2.val:
+                return False
+            queue.append(t1.left)
+            queue.append(t2.right)
+            queue.append(t1.right)
+            queue.append(t2.left)
+        return True
